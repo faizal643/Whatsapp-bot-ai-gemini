52304bfe663c948d0c72f48c5b95ab02ed3b605e
faizal643 <82099112+faizal643@users.noreply.github.com>
2025-01-23 20:29:38 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 5001;
+const port = 8080;
 
 async function mediaToGenerativePart(media: MessageMedia) {
   return {
